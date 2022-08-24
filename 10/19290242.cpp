850d35d08633dc70be627e6bc7187e90119746aa
Ömer Faruk Eren <101640251+omer832@users.noreply.github.com>
2022-08-24 13:52:49 +0300

    readme upgrade

--- 10/19290242.cpp
+++ 10/19290242.cpp
@@ -1,28 +1,54 @@
 #include <iostream>
 using namespace std;
-int main()
-{
 
-    int x, y, sonuc = 0, k, l;   
+void cSort(int dizi[], int k) {
 
-   cin>>x;
-   cin>>y;
+  int output[1000];
+  int count[1000];
+  int max = dizi[0];
 
-    k = x;
-    l = y;
+  for (int i = 1; i < k; i++) {
+    if (dizi[i] > max)
+      max = dizi[i];
+  }
 
-    while(l > 0)
-    {                
-        if(l % 2 != 0)
-        {
-            sonuc += k;
-        }    
+  for (int i = 0; i <= max; ++i) {
+    count[i] = 0;
+  }
 
-        k *= 2;         
-        l /= 2;  
-    }
+  for (int i = 0; i < k; i++) {
+    count[dizi[i]]++;
+  }
 
-    cout<<sonuc<<endl;
+  for (int i = 1; i <= max; i++) {
+    count[i] += count[i - 1];
+  }
 
-    return 0;
+  for (int i = k - 1; i >= 0; i--) {
+    output[count[dizi[i]] - 1] = dizi[i];
+    count[dizi[i]]--;
+  }
+
+  for (int i = 0; i < k; i++) {
+    dizi[i] = output[i];
+  }
+}
+int main() {
+	
+   int n;
+   cin>>n;
+   int dizi[n];
+   for(int i=0;i<n;i++)
+   {	
+   		cin>>dizi[i];		
+   }	
+  cSort(dizi, n);
+  for (int i = 0; i < n; i++){ 
+   cout << dizi[i] << " "; 
+  }
+  
+  
+  
+  return 0;
+  
 }
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # algorithms-with-cpp
+
  Example of the algorithms and math problems with cpp 
+ 
+Bubble Sort
+
+Selection Sort
+
+Insertion Sort
+
+Merge Sort
+
+Quicksort
+
+Counting Sort
+
+Heap Sort
+
+And more
